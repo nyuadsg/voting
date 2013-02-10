ce0df002d649abc312163d4712eca3f1127d14ef
Morgante Pell <morgante.pell@morgante.net>
2013-02-10 10:49:23 +0400

    admin voting

--- models/election.js
+++ models/election.js
@@ -27,13 +27,12 @@ var electionSchema = mongoose.Schema({
 
 electionSchema.methods.vote = function (user, race, candidates) {
 	race= this.races.id( race );
-
 		
 	// check if they are in the proper class
-	if( race.classes.indexOf( user.class ) == -1 )
-	{
-		return false;
-	}
+	// if( race.classes.indexOf( user.class ) == -1 )
+	// {
+	// 	return false;
+	// }
 	
 	// check if they have already voted
 	if( race.voters.indexOf( user.netID ) != -1 )
